2324c6b37ded8c7c6ce655d67d1edde858d10c41
Manav Misra <manavm@visionwebsoft.com>
2021-12-01 20:06:02 -0600

    Add test routes for admin/students

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import config from "./config.js";
+import router from "./routes/index.js";
 
 const app = express();
 
 app.get("/", (_, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api", router);
 
 app.listen(config.port, () => {
   console.log(`Server running: ${config.baseUrl}:${config.port}`);
