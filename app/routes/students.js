809a437521d7c6ab5f4c21e4f8d3b1ea76ab8841
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-12-14 12:11:19 -0600

    feat: :sparkles: get all students
    unprotected

--- app/routes/students.js
+++ app/routes/students.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import studentsController from "../controllers/students.js";
 
 const router = new Router();
 
 router.get("/", (_, res) => {
-  res.send("Hello from Students");
+  res.send("Hello World!");
+});
+
+router.post("/", async (_, res) => {
+  const students = await studentsController.index();
+  res.json(students);
 });
 
 export default router;
