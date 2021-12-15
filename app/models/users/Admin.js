11fcd4d30181f4591416a742928d1fb291cdb78c
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-12-14 20:19:37 -0600

    refactor: :recycle: add default parameters
    prevent crashes

--- app/models/users/Admin.js
+++ app/models/users/Admin.js
@@ -1,6 +1,6 @@
 import User from "./User.js";
 export default class Admin extends User {
-  constructor({ username, password }) {
+  constructor({ username, password } = {}) {
     super({ username, password });
     this.role = "ADMIN";
   }
