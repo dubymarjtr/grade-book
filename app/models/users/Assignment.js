a357e4ac778816574c3ec6f57d4647bff0d85d11
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-12-15 14:15:14 -0600

    feat: :lock: create Assignment model to validate data

---
+++ app/models/users/Assignment.js
@@ -0,0 +1,23 @@
+export default class Assignment {
+  constructor({ title, pointsPossible }) {
+    this.title = title;
+    this.pointsPossible = pointsPossible;
+  }
+
+  validate() {
+    const errors = [];
+    if (!this.title) {
+      errors.push("Title is required");
+    }
+
+    if (!this.pointsPossible) {
+      errors.push("Points possible is required");
+    }
+
+    if (this.pointsPossible < 0) {
+      errors.push("Points possible must be greater than 0");
+    }
+
+    return errors;
+  }
+}
